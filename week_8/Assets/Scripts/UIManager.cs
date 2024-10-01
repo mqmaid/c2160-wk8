d3b17d19e8a90954abf102caeee6bfb6abdcf897
asd917 <65937099+asd917@users.noreply.github.com>
2024-10-01 13:59:51 +1000

    UI / ScoreKeeper

---
+++ week_8/Assets/Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    static private UIManager instance;
+    static public UIManager Instance 
+    {
+        get 
+        {
+            if (instance == null) 
+            {
+                Debug.LogError("There is not UIManager in the scene.");
+            }            
+            return instance;
+        }
+    }
+    [SerializeField] private Text scoreText;
+    [SerializeField] private string scoreFormat = "Current Score: {0}";
+
+    void Awake() 
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        scoreText.text = string.Format(scoreFormat, ScoreKeeper.Instance.Score);
+    }
+}
